aa7eb69859f050932ebfe36fde23d10deec98e25
RayaneMkr <151914264+RayaneMkr@users.noreply.github.com>
2025-01-28 14:12:17 +0100

    Add files via upload

---
+++ JavaFxLMS/src/main/java/com/example/javafxlms/entity/Dossier_patient.java
@@ -0,0 +1,58 @@
+package com.example.javafxlms.entity;
+
+public class Dossier_patient {
+    private int id_dossier_patient;
+    private int date_arrive;
+    private int heure_arrive;
+    private String symptome;
+    private int gravite;
+    private int ref_utilisateur;
+
+    public int getId_dossier_patient() {
+        return id_dossier_patient;
+    }
+
+    public void setId_dossier_patient(int id_dossier_patient) {
+        this.id_dossier_patient = id_dossier_patient;
+    }
+
+    public int getDate_arrive() {
+        return date_arrive;
+    }
+
+    public void setDate_arrive(int date_arrive) {
+        this.date_arrive = date_arrive;
+    }
+
+    public int getHeure_arrive() {
+        return heure_arrive;
+    }
+
+    public void setHeure_arrive(int heure_arrive) {
+        this.heure_arrive = heure_arrive;
+    }
+
+    public String getSymptome() {
+        return symptome;
+    }
+
+    public void setSymptome(String symptome) {
+        this.symptome = symptome;
+    }
+
+    public int getGravite() {
+        return gravite;
+    }
+
+    public void setGravite(int gravite) {
+        this.gravite = gravite;
+    }
+
+    public int getRef_utilisateur() {
+        return ref_utilisateur;
+    }
+
+    public void setRef_utilisateur(int ref_utilisateur) {
+        this.ref_utilisateur = ref_utilisateur;
+    }
+}
